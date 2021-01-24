b74af95d0ea4a69ea29d6d6ac3ae9c8ad8babad9
Bernardo Botto <67174320+BernardoPC-Dev@users.noreply.github.com>
2021-01-24 17:26:58 -0300

    Diminuido o tempo de troca dos leds para facilitar testes.

--- circuitos/semaforo/semaforo.c
+++ circuitos/semaforo/semaforo.c
@@ -12,17 +12,17 @@ void loop()
 {
  digitalWrite(pinR,HIGH);
 
- delay(40000);
+ delay(4000);
 
  digitalWrite(pinR,LOW);
  digitalWrite(pinG,HIGH);
 
- delay(40000);
+ delay(4000);
 
  digitalWrite(pinG,LOW);
  digitalWrite(pinY,HIGH);
 
- delay(7500);
+ delay(2000);
 
  digitalWrite(pinY,LOW);
 }
