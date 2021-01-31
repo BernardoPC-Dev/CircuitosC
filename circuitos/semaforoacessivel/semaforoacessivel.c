2c17da601ce93767ed7d303af44cad1195bb4eb8
Bernardo Botto <67174320+BernardoPC-Dev@users.noreply.github.com>
2021-01-30 22:10:49 -0300

    Update semaforoacessivel.c

--- circuitos/semaforoacessivel/semaforoacessivel.c
+++ circuitos/semaforoacessivel/semaforoacessivel.c
@@ -1,31 +1,32 @@
 int pinR = 2;
 int pinG = 4;
 int pinY = 3;
-int pinB = 5;
+int piezo = 5;
 
 void setup()
 {
  pinMode(pinR,OUTPUT);
  pinMode(pinG,OUTPUT);
  pinMode(pinY,OUTPUT);
- pinMode(pinB,OUTPUT);
+ pinMode(piezo,OUTPUT);
 }
 void loop()
 {
- digitalWrite(pinR,HIGH);
+ digitalWrite(pinR,HIGH); // Vermelho Acende
 
- delay(4000);
+ delay(4000); // 4s
 
- digitalWrite(pinR,LOW);
- digitalWrite(pinG,HIGH);
- digitalWrite(pinB,HIGH);
+ digitalWrite(pinR,LOW); // Vermelho Apaga
+ digitalWrite(pinG,HIGH); // Verde Acende
+ digitalWrite(piezo,HIGH); // Piezo Ativa
 
- delay(4000);
+ delay(4000); // 4s
 
- digitalWrite(pinG,LOW);
- digitalWrite(pinY,HIGH);
- digitalWrite(pinB,LOW);
- delay(2000);
+ digitalWrite(pinG,LOW); // Verde Apaga
+ digitalWrite(piezo,LOW); // Piezo Desativa
+ digitalWrite(pinY,HIGH); // Amarelo Acende
 
- digitalWrite(pinY,LOW);
+ delay(2000); // 2s
+
+ digitalWrite(pinY,LOW); // Amarelo Apaga
 }
